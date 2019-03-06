47cd478c59ce0e88151545e2f6f9ce51a3d772b0
joejaeyeon <joejaeyeon@gmail.com>
2019-03-07 00:23:50 +0900

    catch errors on settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'yplvzkhx0#mu8mel6_kj!#^!0&pw541%glv-&oo8lqel3m9&8&'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'yplvzkhx0#mu8mel6_kj!#^!0&pw541%glv-&oo8lqel3m9&8&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
